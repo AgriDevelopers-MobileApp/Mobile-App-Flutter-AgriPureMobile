2bf1ea0e9640ae57e0ff97480d8e50e811ea9dc1
ClauRozasG <u201922910@upc.edu.pe>
2023-05-28 17:02:15 -0500

    specialist view design

--- lib/presentation/views/specialist_view.dart
+++ lib/presentation/views/specialist_view.dart
@@ -11,20 +11,56 @@ class _SpecialistViewState extends State<SpecialistView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromRGBO(40, 40, 40, 1.0),
         body: Padding(
-          padding: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(12),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Specialist",
+              Text("Do you want to contact an expert?",
                 style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
                 ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
 
-              )
+              Padding(
+                padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Ingrese una categoría para el especialista",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 15,
+              ),
+
+              Text("Specialists contacted: ",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),),
             ],
           ),
         )
+
     );
   }
 }
