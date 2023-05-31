cc962ce03a2c0b50d8fd37320523aef0d9f60293
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2023-05-31 17:16:53 -0500

    feat(authentication): add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'AgriPure App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
