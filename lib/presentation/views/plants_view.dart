1422f9c287dbf7a9adcc81838faf8d2878882004
Gonzalo Sakuda <59516474+Gsak79@users.noreply.github.com>
2023-06-01 09:15:04 -0500

    Add user plants list view and details

--- lib/presentation/views/plants_view.dart
+++ lib/presentation/views/plants_view.dart
@@ -1,3 +1,5 @@
+import 'package:agripure_mobile/presentation/views/plant_detail_view.dart';
+import 'package:agripure_mobile/services/plant_service.dart';
 import 'package:flutter/material.dart';
 
 class PlantsView extends StatefulWidget {
@@ -68,8 +70,110 @@ class _PlantsViewState extends State<PlantsView> {
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
                 ),),
+
+              SizedBox(
+                height: 15,
+              ),
+
+              FutureBuilder(
+                  initialData: [],
+                  future:  PlantService.getPlants(),
+                  builder: (context, AsyncSnapshot<List> snapshot){
+                    return Expanded(
+                      child: ListView.builder(
+                          itemCount: snapshot.data!.length + 1,
+                          itemBuilder: (context, index) {
+                            if (index < snapshot.data!.length) {
+                              var plant = snapshot.data![index];
+
+                              return Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.green,
+                                      borderRadius: BorderRadius.circular(15),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Column(
+                                        children: [
+                                          ClipRRect(
+                                              borderRadius: BorderRadius.circular(15),
+                                              child: Image.network('${plant.image}',
+                                                width: double.infinity,
+                                                height: 200,
+                                                fit: BoxFit.cover,)
+                                          ),
+
+                                          SizedBox(
+                                            height: 20,
+                                          ),
+
+                                          Text("${plant.name}", style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.bold
+                                          ),),
+
+                                          SizedBox(
+                                            height: 20,
+                                          ),
+
+                                          ElevatedButton(
+                                            onPressed: (){
+                                              Navigator.push(context, MaterialPageRoute(
+                                                  builder: (context) => PlantDetailView(plant: plant)
+                                              ));
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.orange, // Establecer el fondo anaranjado
+                                            ),
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(12.0),
+                                              child: Text("Details", style: TextStyle(
+                                                  fontSize: 18,
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold
+                                              ),),
+                                            ),
+                                          )
+
+                                        ],
+                                      ),
+                                    )
+                                ),
+                              );
+                            } else {
+                              return Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Container(
+                                  height: 250,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF4D4D4D),
+                                    borderRadius: BorderRadius.circular(15),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '+ ADD PLANTS',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+
+
+                          }),
+                    );
+                  }
+              )
             ],
           ),
+
         )
     );
   }
