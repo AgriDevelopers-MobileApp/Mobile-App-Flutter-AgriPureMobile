c786317bbcb2b8f3cd82754a2c5ddc96a808ff8c
JostyTafur <89022172+JostyTafur@users.noreply.github.com>
2023-06-14 18:56:51 -0500

    Update home_screen.dart

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -66,7 +66,7 @@ class _HomeScreenState extends State<HomeScreen> {
           BottomNavigationBarItem(
               icon: Icon(Icons.linked_camera_outlined),
               activeIcon: Icon(Icons.linked_camera),
-              label: "Settings",
+              label: "Identify",
               backgroundColor: Colors.green
           ),
 
